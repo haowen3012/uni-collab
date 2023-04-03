97c9741357b6335a4dc11501c19e3fc53c7492ce
haowen3012 <113709699+haowen3012@users.noreply.github.com>
2023-04-03 18:21:34 +0200

    Initial commit

---
+++ src/main/java/it/unicollab/bh/UniCollabApplication.java
@@ -0,0 +1,13 @@
+package it.unicollab.bh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniCollabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniCollabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/unicollab/bh/UniCollabApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unicollab.bh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniCollabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
