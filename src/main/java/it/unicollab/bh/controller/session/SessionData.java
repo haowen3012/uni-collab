322ef764b45da4f082c844219335da57b65d52c8
haowen3012 <113709699+haowen3012@users.noreply.github.com>
2023-04-08 09:08:09 +0200

    continuo della implementazione di sessione

--- src/main/java/it/unicollab/bh/controller/session/SessionData.java
+++ src/main/java/it/unicollab/bh/controller/session/SessionData.java
@@ -1,4 +1,30 @@
 package it.unicollab.bh.controller.session;
 
+import it.unicollab.bh.model.Credentials;
+import it.unicollab.bh.repository.CredentialsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(value="session", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class SessionData {
+
+    private User user;
+
+    private Credentials credentials;
+
+    @Autowired
+    private CredentialsRepository credentialsRepository;
+
+
+    public Credentials getLoggedCredentials(){
+        if(this.credentials == null) {
+        }
+        return this.credentials;
+    }
+
+
 }
