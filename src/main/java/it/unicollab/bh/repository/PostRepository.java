3f16af86d57f8d6f3689c6cf6124ce366beb8ed5
haowen3012 <haowenz01@gmail.com>
2023-05-04 17:45:29 +0200

    no message

--- src/main/java/it/unicollab/bh/repository/PostRepository.java
+++ src/main/java/it/unicollab/bh/repository/PostRepository.java
@@ -3,6 +3,6 @@ package it.unicollab.bh.repository;
 import it.unicollab.bh.model.Post;
 import org.springframework.data.repository.CrudRepository;
 
-public interface PostRepository  extends CrudRepository<Long, Post> {
+public interface PostRepository  extends CrudRepository<Post, Long> {
 
 }
